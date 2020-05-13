d9c222463ad7bf6b852d0d93aa88dfb3d7154144
ahmedmchamed <tunashark@gmail.com>
2020-05-13 18:18:41 +0100

    Extra functions in A and B

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -27,5 +27,7 @@ p stops
 p stops.count()
 
 #8. Show as many ways as you can to return "Falkirk High" from the array?
+p stops[2]
+
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
